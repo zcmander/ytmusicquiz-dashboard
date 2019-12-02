25a338c98b872c77ac1ada52649045d2a5d50eb6
Teijo Mursu <zcmander@gmail.com>
2019-12-02 14:23:29 +0200

    Improved history layout.

--- src/components/QuestionState.tsx
+++ src/components/QuestionState.tsx
@@ -24,24 +24,24 @@ class QuestionState extends Component<Props> {
         const columns: any[] = [];
 
         history.map((event, i) => {
-            if (event.type == "failed")
+            if (event.type === "failed")
             {
-                columns.push(<div className="col bg-danger text-center py-1" key={i}>
-                    <h3>Failed</h3>
+                columns.push(<div className="col bg-danger text-center d-flex justify-content-center align-items-center" key={i}>
+                    <h4>Failed</h4>
                 </div>);
-            } else if (event.type == "player") {
-                columns.push(<div className="col bg-success text-center py-1" key={i}>
-                    <h3>{ event.text }: { event.player }</h3>
+            } else if (event.type === "player") {
+                columns.push(<div className="col bg-success text-center  d-flex justify-content-center align-items-center" key={i}>
+                    <h4>{ event.text }: { event.player }</h4>
                 </div>);
             } else {
-                columns.push(<div className="col bg-info text-center py-1" key={i}>
-                    <h3>{ event.text }</h3>
+                columns.push(<div className="col bg-info text-center  d-flex justify-content-center align-items-center" key={i}>
+                    <h4>{ event.text }</h4>
                 </div>);
             }
         });
 
         for (let i = progress +1; i <= count; i++) {
-            columns.push(<div className="col bg-dark text-center py-1" key={-i}>
+            columns.push(<div className="col bg-dark text-center" key={-i}>
             </div>);
         }
 
